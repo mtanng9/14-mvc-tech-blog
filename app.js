4b3b44af2d282dccbe8279e829350fbada88ef67
My Tang <my@Mys-MacBook-Air.local>
2023-04-27 19:49:06 +0700

    got login and logout working along with the dashboard

--- app.js
+++ app.js
@@ -32,7 +32,11 @@ const sess = {
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(routes);
 
 
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,7 +3,6 @@ const withAuth = require('../utils/auth');
 const { Post } = require('../models');
 
 router.get('/', async (req, res) => {
-    //res.render('home', {layout: 'index'});
     try {
         const postData = await Post.findAll();
     
@@ -22,15 +21,41 @@ router.get('/', async (req, res) => {
 });
 
 router.get('/dashboard', withAuth, async (req, res) => {
-    res.render('dashboard', {layout: 'index'});
+  try {
+    const postData = await Post.findAll({ where: {user_id: req.session.user_id} });
+
+    // Serialize data so the template can read it
+    const posts = postData.map((post) => post.get({ plain: true }));
+
+    // Pass serialized data and session flag into template
+    res.render('home', {
+      posts,
+      logged_in: req.session.logged_in,
+      layout: 'index'
+  });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+    
 });
 
 router.get('/post/:id', async (req, res) => {
-    res.render('post', {layout: 'index'});
+    res.render('post', {
+        logged_in: req.session.logged_in,
+        layout: 'index'
+    });
 });
 
 router.get('/login', (req, res) => {
-    res.render('login', {layout: 'index'});
+    if (req.session.logged_in) {
+        res.redirect('/dashboard');
+        return;
+      }
+    
+      res.render('login', {
+        logged_in: req.session.logged_in,
+        layout: 'index'
+    });
 });
 
 module.exports = router;
