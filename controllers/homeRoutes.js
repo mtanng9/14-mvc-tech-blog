8086e2489830e3ad345dc54d374d8f7e4a9089b2
My Tang <my@Mys-MacBook-Air.local>
2023-04-27 16:01:55 +0700

    routes linked to basic views

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -5,12 +5,15 @@ router.get('/', async (req, res) => {
 });
 
 router.get('/dashboard', async (req, res) => {
+    res.render('dashboard', {layout: 'index'});
 });
 
 router.get('/post/:id', async (req, res) => {
+    res.render('post', {layout: 'index'});
 });
 
 router.get('/login', (req, res) => {
+    res.render('login', {layout: 'index'});
 });
 
 module.exports = router;
